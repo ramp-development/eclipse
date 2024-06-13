b852036a60278b5229a52d50291a223a916cc018
g4knr <85592362+g4knr@users.noreply.github.com>
2024-06-13 17:35:06 +0100

    People slider auto

--- src/components/sliders/people.ts
+++ src/components/sliders/people.ts
@@ -12,31 +12,17 @@ export const people = () => {
   const components = queryElements<HTMLDivElement>(`[${attr}=${value}]`);
   components.forEach((component) => {
     const slider = new Splide(component, {
+      type: 'loop',
+      autoplay: true,
+      interval: 3000,
       arrows: false,
-      pagination: false,
-      // perPage: 1,
       gap: 'calc(var(--style-page--gap) * 2)',
       fixedWidth: '40%',
       breakpoints: {
         991: { fixedWidth: '80%', gap: 'var(--style-page--gap)' },
-        // 767: { fixedWidth: '80%' },
-        // 479: { fixedWidth: '80%' },
       },
     });
 
     slider.mount();
-
-    // checkSplide();
-    // window.onresize = checkSplide;
-
-    // function checkSplide() {
-    //   const mql = window.matchMedia('(max-width: 479px)');
-
-    //   if (mql.matches) {
-    //     slider.mount();
-    //   } else {
-    //     slider.destroy();
-    //   }
-    // }
   });
 };
